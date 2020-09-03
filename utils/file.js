6b43e84c9093bb454611a1204b747c60aac68e03
Simon Tamás <simitomi2003@gmail.com>
2020-09-03 21:38:15 +0200

    Create basic file I/O functionality

---
+++ utils/file.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+const { resolve } = require('path')
+
+module.exports = {
+    readFile,
+    writeFile,
+    readJSON,
+    writeJSON
+}
+
+function readFile(path) {
+    return new Promise((resolve, reject), () => {
+        fs.readFile(path, (err, file) => {
+            resolve(file)
+        })
+    })
+}
+
+function writeFile(path, file) {
+    return new Promise((resolve, reject), () => {
+        fs.writeFile(path, file, err => {
+            resolve(true)
+        })
+    })
+}
+
+function readJSON(path) {
+    return new Promise((resolve, reject), () => {
+        resolve(JSON.parse(await readFile(path)))
+    })
+}
+
+function writeJSON(path, data) {
+    return new Promise((resolve, reject), () => {
+        resolve(await writeFile(path, JSON.stringify(data)))
+    })
+}
