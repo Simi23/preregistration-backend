49fd85f93e108269a5d8751914d19d8fce3274fc
Simon Tamás <simitomi2003@gmail.com>
2020-09-21 20:34:56 +0200

    Extend validating functionality

--- utils/db.js
+++ utils/db.js
@@ -18,6 +18,22 @@ function validateEntry(entry) {
     let validatedEntry = {}
     for(let property in config.properties) {
         if(entry[property.name] == undefined) continue
-        
+        let entryText = entry[property.name]
+        if(typeof(entry[property.name]) != property.type) continue // Add error response
+
+        if(property.sanitize === true) {
+            entryText = sanitizeString(entryText)
+        }
+
+        if(property.hasOwnProperty('pattern')) {
+            let match = entryText.match(property.pattern)
+            if(match == null) continue
+            entryText = match[0]
+        }
     }
+}
+
+function sanitizeString(str){
+    str = str.replace(/[^a-z0-9áéíóöőúüűñ \.,_-]/gim,"");
+    return str.trim();
 }
