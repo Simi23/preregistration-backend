8788061774d05f8e8f48791c8234488fdfa510b7
Simon Tamás <simitomi2003@gmail.com>
2020-09-22 18:35:17 +0200

    Add basic functionalty to test db

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const Config = require('./config')
+const app = require('express')()
+const bodyParser = require('body-parser')
+const db = require('./utils/db')
 
+app.post('/sendEntry', bodyParser.json(), (req, res) => {
+    db.registerEntry(req.body)
+    res.status(200).send()
+})
+
+app.listen(3000)
